e3d79abed38565133759ec99c116744152560a9c
Gloria Jeyech <gjeyech@gmail.com>
2024-08-29 19:53:28 +0300

    pets

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,7 +3,9 @@ import React from "react";
 import Pet from "./Pet";
 
 function PetBrowser() {
-  return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>;
+  return <div className="ui cards">{pets.map((pet) => (
+    <Pet key={pet.id} pet={pet} onAdoptPet={onAdoptPet} />
+  ))}PET COMPONENT SHOULD GO HERE</div>;
 }
 
 export default PetBrowser;

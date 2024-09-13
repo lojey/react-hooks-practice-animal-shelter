66c0adaabb73e62137c1f5df3aaf64e0cf08493e
Gloria Jeyech <gjeyech@gmail.com>
2024-09-13 10:18:58 +0300

    update

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,17 +1,21 @@
 import React from "react";
 
 function Filters({ onChangeType, onFindPetsClick }) {
-  function handleSelectChange(e) {
-    onChangeType(e.target.value);
+  // Handles the change event of the select dropdown
+  function handleSelectChange(event) {
+    onChangeType(event.target.value); // Calls the callback to update the filter type in the App component
   }
-  
-  
-  
+
+  // Handles the click event of the "Find pets" button
+  function handleFindPetsClick() {
+    onFindPetsClick(); // Calls the callback to fetch the pets based on the selected type
+  }
+
   return (
     <div className="ui form">
       <h3>Animal type</h3>
       <div className="field">
-        <select name="type" id="type" aria-label="type">
+        <select name="type" id="type" aria-label="type" onChange={handleSelectChange}>
           <option value="all">All</option>
           <option value="cat">Cats</option>
           <option value="dog">Dogs</option>
@@ -20,7 +24,9 @@ function Filters({ onChangeType, onFindPetsClick }) {
       </div>
 
       <div className="field">
-        <button className="ui secondary button">Find pets</button>
+        <button className="ui secondary button" onClick={handleFindPetsClick}>
+          Find pets
+        </button>
       </div>
     </div>
   );
